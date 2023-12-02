f6ca71525e0b21d827e913a8f73766bf2b91a8b2
Kittenlover229 <artur.roos.artur@gmail.com>
2023-12-02 21:51:14 +0200

    Add solution to part 2

--- 2023/1/main.cpp
+++ 2023/1/main.cpp
@@ -1,10 +1,32 @@
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <string>
 
-#ifdef PART1
+#ifdef PART2
+
+std::optional<std::pair<size_t, char>> slice_matches_words(const char *begin,
+                                                           const char *slice) {
+  static constexpr std::pair<const char *, char> words[10] = {
+      {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
+      {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"ten", 10}};
+  if (begin > slice) {
+    return std::nullopt;
+  }
+
+  for (auto [word, ch] : words) {
+    auto l = std::strlen(word);
+    if (!std::strncmp(slice, word, l))
+      return std::make_pair(std::strlen(word), ch + '0');
+  }
+
+  return std::nullopt;
+}
+
+#endif
+
 int main() {
   std::string buffer;
   unsigned long total = 0;
@@ -14,18 +36,39 @@ int main() {
 
     char n[3] = "\0\0";
 
-    for (int i = 0; !buffer.empty() && std::strlen(n) < 2; i++) {
-      size_t leni = buffer.length() - i - 1;
+    for (int i = 0, j = 0; !buffer.empty() && std::strlen(n) < 2; i++, j++) {
+      size_t leni = buffer.length() - j - 1;
+
       if (n[0] == 0) {
         if ('1' <= buffer[i] && buffer[i] <= '9') {
           n[0] = buffer[i];
         }
+
+#ifdef PART2
+        auto word_match =
+            slice_matches_words(buffer.c_str(), buffer.c_str() + i);
+        if (word_match.has_value()) {
+          auto [offset, value] = word_match.value();
+          n[0] = value;
+          i += offset - 1;
+        }
+#endif
       }
 
       if (n[1] == 0) {
         if ('1' <= buffer[leni] && buffer[leni] <= '9') {
           n[1] = buffer[leni];
         }
+
+#ifdef PART2
+        auto word_match =
+            slice_matches_words(buffer.c_str(), buffer.c_str() + leni);
+        if (word_match.has_value()) {
+          auto [offset, value] = word_match.value();
+          n[1] = value;
+          j += offset - 1;
+        }
+#endif
       }
     }
 
@@ -37,11 +80,3 @@ int main() {
 
   return 0;
 }
-#endif
-
-
-#ifdef PART2
-int main(void) {
-
-}
-#endif
--- run.py
+++ run.py
@@ -2,27 +2,29 @@ from typing import Optional, Literal
 from subprocess import Popen, PIPE, STDOUT
 import os
 
-CPP_BUILD_COMMAND = "clang++ %s -o a.out -O3 -DPART1 -std=c++2b"
+CPP_BUILD_COMMAND = "clang++ %s -o a.out -O3 -DPART%s -std=c++2b"
 
 def main(
-    year: Optional[int],
-    day: Optional[int],
+    year: int,
+    day: int,
+    part: int,
     lang: Literal["rs"] | Literal["cpp"],
 ):
-    folder_path = fr"""./{year}/{day}/"""
+    folder_path = rf"""./{year}/{day}/"""
 
     match lang:
-        case "cpp": 
+        case "cpp":
             exec_path = os.path.join(folder_path, "main.cpp")
-            cmd = CPP_BUILD_COMMAND % exec_path
+            cmd = CPP_BUILD_COMMAND % (exec_path, part)
             os.system(cmd)
             input_path = os.path.join(folder_path, "input.txt")
             with open(input_path) as input:
-                process = Popen('./a.out', stdin=PIPE, stderr=STDOUT, stdout=PIPE)
+                process = Popen("./a.out", stdin=PIPE, stderr=STDOUT, stdout=PIPE)
                 stdout = process.communicate(input=input.read().encode())[0]
                 out = str(stdout.decode()).strip()
                 print(out)
 
 
 if __name__ == "__main__":
-    main(2023, 1, "cpp")
+    main(2023, 1, 1, "cpp")
+    main(2023, 1, 2, "cpp")
